b16800930468d7878b0fcfd9356ee5f97fc6224e
Walmir Pacheco <walmir.pacheco34@gmail.com>
2022-08-24 23:10:51 -0300

    Mensagens nas atualizações e remoções de pedido

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
    <div id="burger-table">
+    <Message :msg="msg" v-show="msg"/>
     <div>
         <div id="burger-table-heading">
             <div class="order-id">#:</div>
@@ -34,14 +35,20 @@
 </template>
 
 <script>
+import Message from './Message.vue';
+
 export default {
     name: "Dashboard",
     data() {
         return {
             burgers: null,
             burger_id: null,
-            status: []
+            status: [],
+            msg: null
         }
+    },
+    components: {
+        Message
     },
     methods: {
         async getPedidos() {
@@ -71,7 +78,10 @@ export default {
 
             const res = await req.json();
 
-            // msg
+            // colocar uma msg de sistema
+            this.msg = `Pedido removido com sucesso!`;
+            // limpar msg
+            setTimeout(() => this.msg = "", 3000);
 
             this.getPedidos();
         },
@@ -88,6 +98,11 @@ export default {
             });
 
             const res = await req.json();
+
+            // colocar uma msg de sistema
+            this.msg = `O pedido Nº${res.id} foi atualizado para: ${res.status}!`;
+            // limpar msg
+            setTimeout(() => this.msg = "", 5000);
 
             console.log(res);
 
